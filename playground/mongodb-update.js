5ee2c214aa3b1362ae5ec287691afd30c3c25827
Matthew Leder <matthew.leder@ledermail.com>
2018-04-19 22:42:07 +0200

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,21 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=> {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5ad8f1bed8d2b949f06f5745')
+  }, {
+    $set: { name: "eva leder" },
+    $inc: { age: 2 }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+//  db.close();
+});
